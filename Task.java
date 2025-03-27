217411f9d0afe65f7359e262391855d6dcadf390
LeNicko <159636950+LeNicko@users.noreply.github.com>
2025-03-27 11:33:08 +0100

    Oui

---
+++ Task.java
@@ -0,0 +1,48 @@
+import java.io.Serializable;
+import java.util.Date;
+
+public class Task implements Serializable {
+    private String title;
+    private String description;
+    private Date dueDate;
+    private boolean isCompleted;
+
+    public Task(String title, String description, Date dueDate) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.isCompleted = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+}
